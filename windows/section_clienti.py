c4a9fc89769049f657bea8e632d7ac82abad3a61
Marcovale22 <matthias.zoldi02@gmail.com>
2025-12-02 17:37:33 +0100

    aggiornata schermata clienti

--- windows/section_clienti.py
+++ windows/section_clienti.py
@@ -5,38 +5,57 @@ from models.cliente import Cliente
 class ClientiSection:
     def __init__(self, ui):
         """
-        ui è la MainWindow: contiene tableClienti, btnClienteAggiungi, ecc.
+        ui è la Ui_MainWindow vera (non MainWindow).
+        Contiene tableClienti, btnClienteAggiungi, ecc.
         """
         self.ui = ui
-
         self.setup_table()
         self.setup_signals()
         self.load_clienti()
 
+    # ---------------------------------------------------------
+    #  SETUP TABELLA
+    # ---------------------------------------------------------
     def setup_table(self):
         table = self.ui.tableClienti
 
-        # 5 colonne: Nome, Cognome, Telefono, Indirizzo, Email
+        # colonne
         table.setColumnCount(5)
         table.setHorizontalHeaderLabels([
             "Nome", "Cognome", "Telefono", "Indirizzo", "Email"
         ])
 
         header = table.horizontalHeader()
+
+        # gestisce automaticamente lo spazio disponibile
         header.setStretchLastSection(True)
-        header.setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
 
-        #numeri a fianco le righe
-        table.verticalHeader().setVisible(False)
+        # permette lo scroll orizzontale quando la finestra è troppo piccola
+        header.setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
 
-        table.setShowGrid(True)
+        # righe alternate
         table.setAlternatingRowColors(True)
 
+        # rimuove numeri riga
+        table.verticalHeader().setVisible(False)
+
+        # se vuoi visibile la griglia
+        table.setShowGrid(True)
+
+        # evita che il contenuto stringa la tabella troppo
+        table.setWordWrap(False)
+
+        # scrollbar abilitate in automatico
+        table.setHorizontalScrollMode(table.ScrollMode.ScrollPerPixel)
+        table.setVerticalScrollMode(table.ScrollMode.ScrollPerPixel)
+
+
     def setup_signals(self):
-        self.ui.btnClienteAggiungi.clicked.connect(self.aggiungi_cliente)
-        # le altre le colleghiamo dopo
-        # self.ui.btnClienteModifica.clicked.connect(self.modifica_cliente)
-        # self.ui.btnClienteElimina.clicked.connect(self.elimina_cliente)
+            self.ui.btnClienteAggiungi.clicked.connect(self.aggiungi_cliente)
+
+            # ---------------------------------------------------------
+            #  CARICAMENTO DATI
+            # ---------------------------------------------------------
 
     def load_clienti(self):
         clienti = Cliente.all()
@@ -51,6 +70,10 @@ class ClientiSection:
             table.setItem(row_idx, 3, QTableWidgetItem(cliente.indirizzo or ""))
             table.setItem(row_idx, 4, QTableWidgetItem(cliente.email or ""))
 
+            # ---------------------------------------------------------
+            #  TASTO AGGIUNGI
+            # ---------------------------------------------------------
+
+
     def aggiungi_cliente(self):
-        # per ora solo debug, poi ci mettiamo la finestra di inserimento
-        print("TODO: aggiungi cliente")
+            print("TODO: aggiungi cliente")
